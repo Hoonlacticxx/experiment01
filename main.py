63c9d31d96b86bd595043ea48ad08e602e2b93fc
Jesus Castillo <castillogonzalezj53@gmail.com>
2026-09-17 11:55:31 -0600

    Load image from URL and implement resizing
    Updated image loading to use a URL and added resizing functionality for the canvas.

--- main.py
+++ main.py
@@ -7,4 +7,29 @@ def load_n_show(dir):
 
   window.geometry("600x400")
 
-  image = Image.open()
+  image = Image.open("https://github.com/Hoonlacticxx/experiment01/blob/main/images.jpg?raw=true")
+  
+  canvas = tk.Canvas(window, highlightthickness=0)
+  canvas.pack(fill = tk.BOTH, expand = True)
+
+  def resize(event):
+    new_len = event.width
+    new_height = event.height
+  
+    if new_len > 10 and new_height > 10:
+      r_image = image.resize(
+        (new_len, new_height), Image.Resampling.LANCZOS
+      )
+  
+      image_tk = ImageTk.PhotoImage(r_image)
+  
+      canvas.image = image.tk
+  
+      canvas.delete("all")
+      canvas.create_image(0, 0, anchor=tk.NW, image=image.tk)
+  
+    canvas.bind("<Configure>", redimesionar_imagen)
+  
+    window.mainloop()
+
+load_n_show("https://github.com/Hoonlacticxx/experiment01/blob/main/images.jpg?raw=true")
