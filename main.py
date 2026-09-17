a30b62c7e8b3b26d88ec109dba13edfed788409d
Jesus Castillo <castillogonzalezj53@gmail.com>
2026-09-17 11:32:40 -0600

    Initialize tkinter window for image display

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from PIL import image, ImageTk
+
+def load_n_show(dir):
+  window = tk.Tk()
+  window.title("activa cam")
+
+  window.geometry("600x400")
+
+  image = Image.open()
